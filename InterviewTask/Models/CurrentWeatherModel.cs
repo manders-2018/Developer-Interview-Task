b9727dd4128f20549ec2fcf82ab3d05d87503407
manders-2018 <melan@DESKTOP-QM35MMR>
2025-01-13 04:02:05 +0000

    Adds model for containing information regarding current weather

---
+++ InterviewTask/Models/CurrentWeatherModel.cs
@@ -0,0 +1,11 @@
+
+namespace InterviewTask.Models
+{
+    public class CurrentWeatherModel
+    {
+        public string RoughLocation { get; set; } = string.Empty;
+        public string CurrentWeather { get; set; } = string.Empty;
+        public string CurrentTemperature { get; set; } = string.Empty;
+    }
+}
+
