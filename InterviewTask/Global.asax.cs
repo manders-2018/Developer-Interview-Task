bec37f0929197e315a1080de16fccf85df45b8ff
manders-2018 <melan@DESKTOP-QM35MMR>
2025-01-13 04:07:05 +0000

    Adds "simple dependency resolver" as this version of .net doesnt have built in DI - which was used in the HomeController

--- InterviewTask/Global.asax.cs
+++ InterviewTask/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InterviewTask.Controllers;
+using InterviewTask.Helpers;
+using InterviewTask.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +19,34 @@ namespace InterviewTask
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DependencyResolver.SetResolver(new SimpleDependencyResolver());
         }
     }
+
+    public class SimpleDependencyResolver : IDependencyResolver
+    {
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(IHelperServiceRepository))
+            {
+                return new HelperServiceRepository();
+            }
+
+            if (serviceType == typeof(HomeController))
+            {
+                var repository = new HelperServiceRepository();
+                SimpleServiceLogger logger = new SimpleServiceLogger();
+                return new HomeController(repository, logger);
+            }
+
+            return null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return new List<object>();
+        }
+    }
+
 }
